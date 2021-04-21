17935058c62532605162820c264b8293e7f85aa0
shubham88fru <shubham88fru@yahoo.com>
2021-04-21 11:48:27 +0530

    Convert App and Search CC to FC

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
@@ -6,54 +6,38 @@ import VideoDetail from './VideoDetail';
 
 import youtube from '../apis/youtube';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onVideoSelect = this.onVideoSelect.bind(this);
-    this.state = { videos: [], selectedVideo: null };
-  }
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  componentDidMount() {
-    this.onTermSubmit('buildings');
-  }
+  useEffect(() => onTermSubmit('buildings'), []);
 
-  onTermSubmit = async (term) => {
+  const onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term,
       },
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
+    await setVideos(response.data.items);
+    await setSelectedVideo(response.data.items[0]);
   };
 
-  onVideoSelect(video) {
-    this.setState({ selectedVideo: video });
-  }
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                videos={this.state.videos}
-                onVideoSelect={this.onVideoSelect}
-              />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,40 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { term: '' };
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('');
 
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-  }
-
-  onInputChange(evt) {
-    this.setState({ term: evt.target.value });
-  }
-
-  onFormSubmit(evt) {
+  const onSubmit = (evt) => {
     evt.preventDefault();
-    this.props.onFormSubmit(this.state.term);
-  }
+    onFormSubmit(term);
+  };
 
-  render() {
-    return (
-      <div className="ui segment serch-bar">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label htmlFor="search-input">Video Search</label>
-            <input
-              id="search-input"
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ui segment serch-bar">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label htmlFor="search-input">Video Search</label>
+          <input
+            id="search-input"
+            type="text"
+            value={term}
+            onChange={(evt) => setTerm(evt.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
